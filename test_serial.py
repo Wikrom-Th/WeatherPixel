162532452918f916fd46e35f498173641c45dece
Wikrom Thamchareonkit <59578780+Wikrom-Th@users.noreply.github.com>
2023-06-16 14:06:49 +0900

    Update test_serial.py

--- test_serial.py
+++ test_serial.py
@@ -5,9 +5,10 @@ from src.serial_com import WeatherSerial
 
 myserial = WeatherSerial(COM_PORT)
 
-for i in range(0, 40):
+for i in range(0,41,5):
     myserial.write_temp(i)
     print(i)
     sleep(0.5)
 
+sleep(5)
 myserial.write_temp(25, 1)
